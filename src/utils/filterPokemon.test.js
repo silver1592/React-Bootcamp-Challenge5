c78d9afe190d07c53afe79a46f0426c6519930b8
Fernando Alvarez Flores <feral1592@gmail.com>
2021-06-24 01:40:15 -0500

    Empty array test

--- src/utils/filterPokemon.test.js
+++ src/utils/filterPokemon.test.js
@@ -9,15 +9,17 @@ describe("Filter Pokemon", () => {
     expect(Array.isArray(result)).toBe(true);
   });
 
-  // test("it should return an empty array", () => {
-  //   /* Your solution comes here */
-  //   expect(false).toBe(true);
-  // });
+  test("it should return an empty array", () => {
+    /* Your solution comes here */
+    const result = filterPokemon(mockPokemonsData, "");
+    expect(result).toEqual([]);
+  });
 
-  // test("it should return an array with charizard object", () => {
-  //   /* Your solution comes here */
-  //   expect(false).toBe(true);
-  // });
+  test("it should return an array with charizard object", () => {
+    /* Your solution comes here */
+    const result = filterPokemon(mockPokemonsData, "charizard");
+    expect(result).toContain(mockPokemonsData.charizard);
+  });
 
   // /* BUNUS */
   // test("it should validate the 'pokeList' argument", () => {
