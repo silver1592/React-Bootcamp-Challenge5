3c020d610cb21ccbaa6e986f17cf711edad8899f
Fernando Alvarez Flores <feral1592@gmail.com>
2021-06-24 01:11:53 -0500

    Initial commit

---
+++ src/utils/filterPokemon.test.js
@@ -0,0 +1,30 @@
+import filterPokemon from "./filterPokemon";
+import { mockPokemonsData } from "../mock/pokeData";
+
+describe("Filter Pokemon", () => {
+  test("it should return an array", () => {
+    /* Your solution comes here */
+    expect(false).toBe(true);
+  });
+
+  test("it should return an empty array", () => {
+    /* Your solution comes here */
+    expect(false).toBe(true);
+  });
+
+  test("it should return an array with charizard object", () => {
+    /* Your solution comes here */
+    expect(false).toBe(true);
+  });
+
+  /* BUNUS */
+  test("it should validate the 'pokeList' argument", () => {
+    /* Your solution comes here */
+    expect(false).toBe(true);
+  });
+
+  test("it should validate the 'name' argument", () => {
+    /* Your solution comes here */
+    expect(false).toBe(true);
+  });
+});
