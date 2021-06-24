84fb76e10aeb0582dddc0c0d1090a1708ef74c1a
Fernando Alvarez Flores <feral1592@gmail.com>
2021-06-24 01:29:40 -0500

    Return an Array

--- src/utils/filterPokemon.test.js
+++ src/utils/filterPokemon.test.js
@@ -4,27 +4,29 @@ import { mockPokemonsData } from "../mock/pokeData";
 describe("Filter Pokemon", () => {
   test("it should return an array", () => {
     /* Your solution comes here */
-    expect(false).toBe(true);
+    const result = filterPokemon(mockPokemonsData, "");
+
+    expect(Array.isArray(result)).toBe(true);
   });
 
-  test("it should return an empty array", () => {
-    /* Your solution comes here */
-    expect(false).toBe(true);
-  });
+  // test("it should return an empty array", () => {
+  //   /* Your solution comes here */
+  //   expect(false).toBe(true);
+  // });
 
-  test("it should return an array with charizard object", () => {
-    /* Your solution comes here */
-    expect(false).toBe(true);
-  });
+  // test("it should return an array with charizard object", () => {
+  //   /* Your solution comes here */
+  //   expect(false).toBe(true);
+  // });
 
-  /* BUNUS */
-  test("it should validate the 'pokeList' argument", () => {
-    /* Your solution comes here */
-    expect(false).toBe(true);
-  });
+  // /* BUNUS */
+  // test("it should validate the 'pokeList' argument", () => {
+  //   /* Your solution comes here */
+  //   expect(false).toBe(true);
+  // });
 
-  test("it should validate the 'name' argument", () => {
-    /* Your solution comes here */
-    expect(false).toBe(true);
-  });
+  // test("it should validate the 'name' argument", () => {
+  //   /* Your solution comes here */
+  //   expect(false).toBe(true);
+  // });
 });
